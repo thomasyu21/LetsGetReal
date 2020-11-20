dc11426d164b4c6a049ba056ebe445c4c00d1f52
thomasyu21 <thomasyu085@gmail.com>
2020-11-20 17:44:59 -0500

    Created the constructor for RationalNumber.

---
+++ RationalNumber.java
@@ -0,0 +1,17 @@
+public class RationalNumber extends RealNumber{
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno){
+    super(0.0);
+    if ((deno < 0 && nume > 0) || (nume < 0 && deno < 0)){
+      numerator = -nume;
+      denominator = -deno;
+    }else if (deno == 0){
+      numerator = 0;
+      denominator = 1;
+    }else{
+    numerator = nume;
+    denominator = deno;
+    }
+  }
+}
