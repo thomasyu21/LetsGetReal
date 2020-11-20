05fb035d6d336d44d70c70bbcb501bfa5af69e74
thomasyu21 <thomasyu085@gmail.com>
2020-11-20 17:34:08 -0500

    Updated equals method to account for a value of zero. Created add and multiply methods for RealNumbers.

--- RealNumber.java
+++ RealNumber.java
@@ -14,10 +14,27 @@ public class RealNumber{
   }
 
   public boolean equals(RealNumber other){
+    if (this.value == 0 || other.value == 0) {
+      if (this.value == other.value) {
+        return true;
+      }else{
+        return false;
+      }
+    }
     double difference = Math.abs(((this.value-other.value)/this.value)*100);
     if (difference >= 0.001) {
       return true;
     }
     return false;
+  }
+
+  public RealNumber add(RealNumber other){
+    RealNumber result = new RealNumber(this.value+other.value);
+    return result;
+  }
+
+  public RealNumber multiply(RealNumber other){
+    RealNumber result = new RealNumber(this.value*other.value);
+    return result;
   }
 }
--- Tester.java
+++ Tester.java
@@ -2,10 +2,24 @@ public class Tester{
   public static void main(String[] args){
     RealNumber a = new RealNumber(0.001);
     RealNumber b = new RealNumber(0.002);
+    RealNumber c = new RealNumber(-0.001);
+    RealNumber d = new RealNumber(0.0);
+    RealNumber e = new RealNumber(0.0);
+    RealNumber f = new RealNumber(1.0);
 
     System.out.println(a.getValue());
     System.out.println(b.getValue());
 
     System.out.println(a.equals(b));
+    System.out.println(a.equals(d));
+    System.out.println(e.equals(d));
+
+    System.out.println(a.add(b));
+    System.out.println(c.add(b));
+
+    System.out.println(a.multiply(b));
+    System.out.println(d.multiply(b));
+    System.out.println(a.multiply(f));
+    System.out.println(c.multiply(f));
   }
 }
